22d4f217cb7d29ed29c73f8b2cc56a2fbd205cc8
stratisluc (El) <eleassarf@gmail.com>
2025-06-09 01:41:56 -0400

    Update Navbar.jsx
    literally just did all that work for no reason smh, lmfao found out an easier way of doing the shit i just spent a whole hour working on

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,82 +9,84 @@ function Navbar() {
 
   return (
     <>
-      <div className=" py-8 fixed top-24 w-full z-50 bg-gradient-to-b from-black via-black/90 via-50% to-transparent">
-        <nav className="bg-black py-4 fixed top-0 w-full z-50 ">
-          <div className="container mx-auto px-4 py-4 relative">
-            <button
-              className="block md:hidden text-white focus:outline-none"
-              onClick={toggleMenu}
-              aria-label="Toggle navigation menu"
+      {/* via-black/(x) how dark...*/}
+      {/* via-(x)% ...at which point*/}
+      {/* <div className="py-6 fixed top-20 w-full z-50 bg-gradient-to-b from-black via-black/100 via-15% to-transparent"> */}
+      <nav className=" py-4 fixed top-0 w-full z-50 bg-gradient-to-b from-black via-black/100 via-80% to-transparent">
+        <div className="container mx-auto px-4 py-4 relative">
+          <button
+            className="block md:hidden text-white focus:outline-none"
+            onClick={toggleMenu}
+            aria-label="Toggle navigation menu"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 12h16"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 18h16"
-                />
-              </svg>
-            </button>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 12h16"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 18h16"
+              />
+            </svg>
+          </button>
 
-            <div className="hidden md:flex items-center justify-center space-x-4 ">
-              <a
-                href="#hero"
-                className="px-3 py-2 text-lg text-white transition transform hover:scale-110
+          <div className="hidden md:flex items-center justify-center space-x-4 ">
+            <a
+              href="#hero"
+              className="px-3 py-2 text-lg text-white transition transform hover:scale-110
                     active:bg-white active:text-black rounded-md"
-              >
-                Home
-              </a>
-              <a
-                href="#about"
-                className="px-3 py-2 text-lg text-white transition transform hover:scale-110
+            >
+              Home
+            </a>
+            <a
+              href="#about"
+              className="px-3 py-2 text-lg text-white transition transform hover:scale-110
                     active:bg-white active:text-black rounded-md"
-              >
-                About
-              </a>
-              <a
-                href="#projects"
-                className="px-3 py-2 text-lg text-white transition transform hover:scale-110
+            >
+              About
+            </a>
+            <a
+              href="#projects"
+              className="px-3 py-2 text-lg text-white transition transform hover:scale-110
                     active:bg-white active:text-black rounded-md"
-              >
-                Projects
-              </a>
-              <a
-                href="#resume"
-                className="px-3 py-2 text-lg text-white transition transform hover:scale-110
+            >
+              Projects
+            </a>
+            <a
+              href="#resume"
+              className="px-3 py-2 text-lg text-white transition transform hover:scale-110
                     active:bg-white active:text-black rounded-md"
-              >
-                Resume
-              </a>
-              <a
-                href="#Contact"
-                className="px-3 py-2 text-lg text-white transition transform hover:scale-110
+            >
+              Resume
+            </a>
+            <a
+              href="#Contact"
+              className="px-3 py-2 text-lg text-white transition transform hover:scale-110
                     active:bg-white active:text-black rounded-md"
-              >
-                Contact
-              </a>
-            </div>
+            >
+              Contact
+            </a>
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
+      {/* </div> */}
 
       <div
         className={`bg-white text-black overflow-hidden w-full fixed top-16 z-40 transition-all duration-300 ease-in-out md:hidden ${
