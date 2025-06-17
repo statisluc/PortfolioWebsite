6ec865bdb499b4e3b249f65501a6e4cf4b5935f9
stratisluc (El) <eleassarf@gmail.com>
2025-06-17 16:52:08 -0400

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,6 +7,19 @@ function Project() {
         <h2 className="text-3xl font-bold text-black mb-2">Projects</h2>
         <p className="text-lg text-gray-800">I dont know what to do no more</p>
       </Card>
+      <div className="pt-8 max-w-[300px]">
+        <Card>
+          <p className="text-lg text-gray-800">
+            Quick little test
+            <br />
+            and when you expand onto the note, we see how long it goes and when
+            you expand onto the note, we see how long it goes and when you
+            expand onto the note, we see how long it goes and when you expand
+            onto the note, we see how long it goes and when you expand onto the
+            note, we see how long it goes
+          </p>
+        </Card>
+      </div>
     </div>
   );
 }
