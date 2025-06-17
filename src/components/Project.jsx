d3b1dbb7030325fb2f553f42d904ac2eb68a8a7f
stratisluc (El) <eleassarf@gmail.com>
2025-06-17 18:28:48 -0400

    Update Project.jsx
    adding more cards into project.jsx to showcase my other projects. formatting, creating new divs, etc.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,42 +3,60 @@ import Card from "./Card";
 function Project() {
   return (
     <div className="w-full max-w-screen-md mx-auto px-4 min-h-screen flex flex-col justify-center text-center items-center">
-      <Card>
-        <h2 className="text-3xl font-bold text-black mb-2">Projects</h2>
-        <p className="text-lg text-gray-800">I dont know what to do no more</p>
-      </Card>
-      <div className="pt-8 max-w-[300px]">
+      <div className="mt-10 w-full">
         <Card>
+          <h2 className="text-3xl font-bold text-black mb-2">Projects</h2>
           <p className="text-lg text-gray-800">
-            Quick little test
-            <br />
-            and when you expand onto the note, we see how long it goes and when
-            you expand onto the note, we see how long it goes and when you
-            expand onto the note, we see how long it goes and when you expand
-            onto the note, we see how long it goes and when you expand onto the
-            note, we see how long it goes
+            I dont know what to do no more
           </p>
         </Card>
       </div>
-      <div className="pt-8 max-w-[300px]">
-        <Card>
-          <p className="text-lg text-gray-800">
-            Quick little testand when you expand onto the note, we see how long
-            it goes and when you expand onto the note, we see how long it goes
-            and when you expand onto the note, we see how long it goes and when
-            you expand onto the note, we see how long it goes and when you
-            expand onto the note, we see how long it goesand when you expand
-            onto the note, we see how long it goes and when you expand onto the
-            note, we see how long it goes and when you expand onto the note, we
-            see how long it goes and when you expand onto the note, we see how
-            long it goes and when you expand onto the note, we see how long it
-            goesand when you expand onto the note, we see how long it goes and
-            when you expand onto the note, we see how long it goes and when you
-            expand onto the note, we see how long it goes and when you expand
-            onto the note, we see how long it goes and when you expand onto the
-            note, we see how long it goes
-          </p>
-        </Card>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full mb-10">
+        <div className="pt-8 max-w-[300px]">
+          <Card>
+            <p className="text-lg text-gray-800">
+              Quick little test
+              <br />
+              and when you expand onto the note, we see how long it goes and
+              when you expand onto the note, we see how long it goes and when
+              you expand onto the note, we see how long it goes and when you
+              expand onto the note, we see how long it goes and when you expand
+              onto the note, we see how long it goes
+            </p>
+          </Card>
+        </div>
+        <div className="pt-8 max-w-[300px]">
+          <Card>
+            <p className="text-lg text-gray-800">
+              Quick little testand when you expand onto the note, we see how
+              long it goes and when you expand onto the note, we see how long it
+              goes and when you expand onto the note, we see how long it goes
+              and when you expand onto the note, we see how long it goes and
+              when you expand onto the note, we see how long it goesand when you
+              expand onto the note, we see how long it goes and when you expand
+              onto the note, we see how long it goes and when you expand onto
+              the note, we see how long it goes and when you expand onto the
+              note, we see how long it goes and when you expand onto the note,
+              we see how long it goesand when you expand onto the note, we see
+              how long it goes and when you expand onto the note, we see how
+              long it goes and when you expand onto the note, we see how long it
+              goes and when you expand onto the note, we see how long it goes
+              and when you expand onto the note, we see how long it goes
+            </p>
+          </Card>
+        </div>
+        <div className="pt-8 max-w-[300px]">
+          <Card>
+            <p className="text-lg text-gray-800">
+              Quick test random computer programming jargon like array list note
+              tuple jargon like array list note tuplejargon like array list note
+              tuplejargon like array list note tuplejargon like array list note
+              tuplejargon like array list note tuplejargon like array list note
+              tuplejargon like array list note tuple
+            </p>
+          </Card>
+        </div>
       </div>
     </div>
   );
