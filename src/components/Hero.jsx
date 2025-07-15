8cc9d0ba06c5b5cae2f039edb9954b4cb073c1e5
Alex <eleassarf@gmail.com>
2025-07-15 11:38:48 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ function Hero() {
   return (
     <section className="relative w-full max-w-screen-md mx-auto px-4 min-h-screen flex flex-col items-center justify-center text-center">
       <h1 className="text-5xl sm:text-6xl font-bold tracking-tight text-white">
-        Eleassar Famutimi
+        Eleassar (Alexander Valdez) Famutimi
       </h1>
       <p className="mt-4 text-lg sm:text-xl max-w-xl text-gray-300">
         Computer Science Student With An Interest In iOS, Website, and Android
